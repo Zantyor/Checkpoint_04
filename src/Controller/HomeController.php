a739bfb35a747ba81d1e474a828e27edd7d0b509
Côme de Mauroy <demauroyco@gmail.com>
2020-01-30 09:50:09 +0100

    WIP dynamic infos front

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogRepository;
+use App\Repository\InfosRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\SkillsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
+     * @param BlogRepository $blogRepository
+     * @param InfosRepository $infosRepository
+     * @param ProjectRepository $projectRepository
+     * @param SkillsRepository $skillsRepository
+     * @return Response
      */
-    public function index()
+    public function index(
+        BlogRepository $blogRepository,
+        InfosRepository $infosRepository,
+        ProjectRepository $projectRepository,
+        SkillsRepository $skillsRepository
+    )
     {
+        $allProjects = $projectRepository->findAll();
+        $allSkills = $skillsRepository->findAll();
+        $articles = $blogRepository->findAll();
+        $infos = $infosRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'projects' => $allProjects,
+            'skills' => $allSkills,
+            'articles' => $articles,
+            'infos' => $infos
         ]);
     }
 }
